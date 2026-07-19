e90ec4633e06cf818cc4cea6ec0400e93077b0f0
Odunayo Damilola <odunayodamilola2001@gmail.com>
2026-07-19 16:04:55 +0100

    Add Task 2

---
+++ Tasks/Task2.js
@@ -0,0 +1,6 @@
+console.log("JavaScript is a programming language used to implement complex features on web pages.");
+console.log("It is built into modern browsers.");
+console.log( "It is the default language for web programming.");
+console.log("JavaScript is dynamically typed.");
+console.log( "The type of variable used is checked during run-time.");
+console.log("JavaScript was created in just 10 days by Brendan Eich in 1995.");
