757bf1d7e327c04aa0edff8a95af2670d73802ca
Odunayo Damilola <odunayodamilola2001@gmail.com>
2026-07-24 21:05:18 +0100

    Add Task 2

--- Tasks/Task2.js
+++ Tasks/Task2.js
@@ -1,6 +1,8 @@
-console.log("JavaScript is a programming language used to implement complex features on web pages.");
-console.log("It is built into modern browsers.");
-console.log( "It is the default language for web programming.");
-console.log("JavaScript is dynamically typed.");
-console.log( "The type of variable used is checked during run-time.");
-console.log("JavaScript was created in just 10 days by Brendan Eich in 1995.");
+/*
+1. JavaScript is a programming language used to implement complex features on web pages.
+2. It is built into modern browsers.
+3. It is the default language for web programming.
+4. JavaScript is dynamically typed.
+5. The type of variable used is checked during run-time.
+6. JavaScript was created in just 10 days by Brendan Eich in 1995.
+*/
