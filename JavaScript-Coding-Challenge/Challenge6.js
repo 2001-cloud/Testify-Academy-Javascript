7110d748ec3b95c61ab0fbae467dc12ee22f2e70
Odunayo Damilola <odunayodamilola2001@gmail.com>
2026-08-01 08:03:07 +0100

    Challenge6.js

---
+++ JavaScript-Coding-Challenge/Challenge6.js
@@ -0,0 +1,7 @@
+const names = ["Ope", "Tunde", "Damilola", "Alice", "Bob", "Charlie", "David", "Eve"];
+
+function sortStrings(array) {
+  return array.sort();
+}
+
+console.log(sortStrings(names));
